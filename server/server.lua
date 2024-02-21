2d10ef70f42f371de7d0bbf402b843caec2a5dc4
DMALProject <128133788+DMALProject@users.noreply.github.com>
2024-02-21 23:28:10 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -7,12 +7,3 @@ lib.addCommand(Config.Settings.Command, {
 }, function(source, args, raw)
     TriggerClientEvent('dmal_staffText:StaffMode', source)
 end)
-
-AddEventHandler('onResourceStart', function()
-    if (GetCurrentResourceName() ~= "dmal_staffText") then
-        while true do
-            Citizen.Wait(5000)
-            print('(ERROR) dmal_staffText : don\'t rename the resource')
-        end
-    end
-end)
